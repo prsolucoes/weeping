fca1504ef6bc6a86df93ee85b3f5562772ce0d90
Lo-X <loic.boutter@gmail.com>
2014-01-31 18:21:12 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "application.h"
+
+using namespace std;
+
+int main()
+{
+    const unsigned int width = 1024, height = width * 1/2;
+    try {
+        Application game(width, height, "Weeping");
+        game.run();
+    } catch(std::exception& e) {
+        std::cerr << "Game ended unexpectedely : " << std::endl;
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
+}
+
---
+++ states/state.h
@@ -0,0 +1,60 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <memory>
+
+#include "stateidentifiers.h"
+#include "../resources/resourceidentifiers.h"
+#include "../resources/resourceholder.h"
+#include "../resources/musicplayer.h"
+#include "../resources/soundplayer.h"
+#include "../resources/scriptplayer.h"
+
+class StateStack;
+class PlayerController;
+
+class State
+{
+public:
+    typedef std::unique_ptr<State> Ptr;
+
+    struct Context
+    {
+        Context(sf::RenderWindow& window, TextureHolder& textures, FontHolder& fonts, SoundPlayer& sounds, MusicPlayer& music, ScriptPlayer& scripts, PlayerController& player);
+
+        sf::RenderWindow*   window;
+        TextureHolder*      textures;
+        FontHolder*         fonts;
+        SoundPlayer*        sounds;
+        MusicPlayer*        music;
+        ScriptPlayer*       scripts;
+        PlayerController*   player;
+    };
+
+public:
+    State(StateStack& stack, Context context);
+    virtual         ~State();
+
+    virtual void    draw() = 0;
+    virtual bool    update(sf::Time dt) = 0;
+    virtual bool    handleEvent(const sf::Event& event) = 0;
+
+    virtual void    onPause();
+    virtual void    onResume();
+
+protected:
+    void            requestStackPush(States::ID id);
+    void            requestStackPop();
+    void            requestStackClear();
+
+    Context         getContext() const;
+
+private:
+    StateStack*     mStack;
+    Context         mContext;
+};
+
+#endif // STATE_H
